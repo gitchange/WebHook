31f8f93539f226d91c3d0c34ac364fb3c3b6a7c7
change.wu <change.wuu@gmail.com>
2017-06-26 20:26:24 +0800

    S201706262026

--- WebHook/Controllers/LineChatController.cs
+++ WebHook/Controllers/LineChatController.cs
@@ -1,23 +1,97 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using WebHook.Models;
 
 namespace WebHook.Controllers
 {
     public class LineChatController : ApiController
     {
+
+        protected string ChannelAccessToken = "JZilAUe52oNviAO0pnqMs/Ukz+kumvYmQFExILnBdB/PfBtgITd8TRbbT1Zdakn9mCwQ4QdSHYz78QqCI8gpUWJ5Kv4E23iqlgWV0HmWecXGU/CO3S4lfbYFlb10m1sWi184xfuujnJ4y1WXvJFwMwdB04t89/1O/w1cDnyilFU=";
+        protected string myLineID = "Ub7edd29f9ec12e41b1eae1c11baa733d";
+        protected isRock.LineBot.ReceievedMessage ReceivedMessage;
+        protected isRock.LineBot.Bot LintBot;
+
         [HttpPost]
-        public IHttpActionResult POST()
+        public async Task<IHttpActionResult> POSTAsync()
         {
-            string ChannelAccessToken = "JZilAUe52oNviAO0pnqMs/Ukz+kumvYmQFExILnBdB/PfBtgITd8TRbbT1Zdakn9mCwQ4QdSHYz78QqCI8gpUWJ5Kv4E23iqlgWV0HmWecXGU/CO3S4lfbYFlb10m1sWi184xfuujnJ4y1WXvJFwMwdB04t89/1O/w1cDnyilFU=";
+            try
+            {
+                //取得 http Post RawData(should be JSON)
+                string postData = Request.Content.ReadAsStringAsync().Result;
+                //剖析JSON
+                ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
+                //建立 Line BOT
+                LintBot = new isRock.LineBot.Bot(ChannelAccessToken);
+                //取得 User 所 PO 的訊息
+                string userMsg = ReceivedMessage.events[0].message.text;
+
+                //新朋友來了(或解除封鎖)
+                if (ReceivedMessage.events.FirstOrDefault().type == "follow" || ReceivedMessage.events.FirstOrDefault().type == "join")
+                {
+                    NewJoin();
+                }
+
+                //專門處理關鍵字 - "PM2.5"
+                if (userMsg.ToUpper().Contains("PM2.5") || userMsg.Contains("空氣品質") || userMsg.Contains("空污"))
+                {
+                    await GetAirQulity(userMsg.ToUpper());
+                }
+
+                //專門處理關鍵字 - "里長嬤"
+                if (userMsg.Contains("里長嬤"))
+                {
+                    District();
+                }
+                //回覆API OK
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return Ok();
+            }
+        }
+
+        #region 新朋友來了(或解除封鎖)
+        /// <summary>
+        /// 新朋友來了(或解除封鎖)
+        /// </summary>
+        private void NewJoin()
+        {
+            var userInfo = LintBot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.userId);
+            var groupInfo = LintBot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.groupId);
+            var roomInfo = LintBot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.roomId);
+
+            //bot.ReplyMessage(ReceivedMessage.events.FirstOrDefault().replyToken, $"哈，'{userInfo.displayName}' 你來了...歡迎");
+
+            LintBot.ReplyMessage(ReceivedMessage.events.FirstOrDefault().replyToken, $"哈囉！'{userInfo.displayName}'，我回來了...GroupID='{groupInfo.userId}'");
+
+            LintBot.PushMessage(myLineID, $"UserName='{userInfo.displayName}' , UserID='{userInfo.userId}' ");
+            LintBot.PushMessage(myLineID, $"GroupName='{groupInfo.displayName}' , GroupID='{groupInfo.userId}' ");
+            LintBot.PushMessage(myLineID, $"RoomName='{roomInfo.displayName}' , RoomID='{roomInfo.userId}' ");
+        }
+        #endregion
+
+        #region 專門處理關鍵字 - "里長嬤"
+        /// <summary>
+        /// 專門處理關鍵字 - "里長嬤"
+        /// </summary>
+        private void District()
+        {
             int nMsgNumber = 10;
             string[] ResponseMessage = new string[nMsgNumber];
+            string Message;
             Random random = new Random();
             int current_random = 0;
 
+            // 取得隨機要回覆的訊息
+            current_random = random.Next(0, 10);
             ResponseMessage[0] = "你今天還好嗎？";
             ResponseMessage[1] = "你今天有運動嗎？";
             ResponseMessage[2] = "不要再吃了哦...";
@@ -29,49 +103,72 @@ namespace WebHook.Controllers
             ResponseMessage[8] = "別再睡了！";
             ResponseMessage[9] = "又敗家了嗎？";
 
+            //回覆訊息
+            Message = "里長嬤，" + ResponseMessage[current_random];
+            //回覆用戶
+            isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
+
+            // 暫時測試
+            //isRock.LineBot.Bot bot = new isRock.LineBot.Bot(ChannelAccessToken);
+            //var userInfo = bot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.userId);
+            //bot.PushMessage(myLineID, $"UserName='{userInfo.displayName}' , UserID='{userInfo.userId}' ");
+        }
+        #endregion
+
+        #region 專門處理關鍵字 - "PM2.5"
+        /// <summary>
+        /// 專門處理關鍵字 - "PM2.5"
+        /// </summary>
+        public async Task GetAirQulity(string msg)
+        {
+            const string targetURL = "http://opendata.epa.gov.tw/ws/Data/REWXQA/?%24orderby=SiteName&%24skip=0&%24top=1000&format=json";
             try
             {
-                // 取得隨機要回覆的訊息
-                current_random = random.Next(0, 10);
-                //取得 http Post RawData(should be JSON)
-                string postData = Request.Content.ReadAsStringAsync().Result;
-                //剖析JSON
-                var ReceivedMessage = isRock.LineBot.Utility.Parsing(postData);
-
-                if (ReceivedMessage.events.FirstOrDefault().type == "follow" || ReceivedMessage.events.FirstOrDefault().type == "join")
+                using (HttpClient client = new HttpClient())
                 {
-                    //新朋友來了(或解除封鎖)
-                    isRock.LineBot.Bot bot = new isRock.LineBot.Bot(ChannelAccessToken);
-                    var userInfo = bot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.userId);
-                    var groupInfo = bot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.groupId);
-                    var roomInfo = bot.GetUserInfo(ReceivedMessage.events.FirstOrDefault().source.roomId);
 
-                    //bot.ReplyMessage(ReceivedMessage.events.FirstOrDefault().replyToken, $"哈，'{userInfo.displayName}' 你來了...歡迎");
+                    client.MaxResponseContentBufferSize = Int32.MaxValue;
+                    var response = await client.GetStringAsync(targetURL);
+                    var collection = JsonConvert.DeserializeObject<IEnumerable<AirQulity>>(response.Replace("PM2.5", "PM25"));
+                    var result = (from c in collection
+                                  where msg.Contains(c.SiteName)
+                                  select c);
+                    if (result.Any())
+                    {
+                        string remsg = string.Empty;
+                        string recommend = string.Empty;
+                        int nloop = 0;
+                        foreach (var rr in result)
+                        {
+                            if (nloop > 0) remsg = remsg + "%0D%0A";
+                            int intPM25 = int.Parse(rr.PM25);
+                            if (intPM25 >= 0 || intPM25 <= 35)
+                                recommend = "(舒適，可從事戶外活動)";
+                            else
+                                if (intPM25 >= 36 || intPM25 <= 53)
+                                recommend = "(舒適尚可，可從事戶外活動，但應考慮減少體力消耗)";
+                            else
+                                if (intPM25 >= 54 || intPM25 <= 70)
+                                recommend = "(任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應該考慮減少戶外活動)";
+                            else
+                                if (intPM25 >= 71) recommend = "(任何人如果有不適，如眼痛，咳嗽或喉嚨痛等，應減少體力消耗，特別是減少戶外活動)";
 
-                    bot.ReplyMessage(ReceivedMessage.events.FirstOrDefault().replyToken, $"哈囉！'{userInfo.displayName}'，我回來了...GroupID='{groupInfo.userId}'");
-
-                    isRock.LineBot.Utility.PushMessage("Ub7edd29f9ec12e41b1eae1c11baa733d", $"UserName='{userInfo.displayName}' , UserID='{userInfo.userId}' ", ChannelAccessToken);
-                    isRock.LineBot.Utility.PushMessage("Ub7edd29f9ec12e41b1eae1c11baa733d", $"GroupName='{groupInfo.displayName}' , GroupID='{groupInfo.userId}' ", ChannelAccessToken);
-                    isRock.LineBot.Utility.PushMessage("Ub7edd29f9ec12e41b1eae1c11baa733d", $"RoomName='{roomInfo.displayName}' , RoomID='{roomInfo.userId}' ", ChannelAccessToken);
-
-                    return Ok();
+                            remsg = remsg + string.Format("{0}的 PM2.5數值為{1} {2}", rr.SiteName, rr.PM25, recommend);
+                        }
+                        LintBot.ReplyMessage(ReceivedMessage.events[0].replyToken, remsg);
+                    }
+                    else
+                    {
+                        LintBot.ReplyMessage(ReceivedMessage.events[0].replyToken, "無法識別你所指定的地區，總之，快去買一台 Cado 回家就對了...");
+                    }
                 }
-
-                string Message;
-                if (ReceivedMessage.events[0].message.text.Contains("里長嬤"))
-                {
-                    //回覆訊息
-                    Message = "里長嬤，" + ResponseMessage[current_random];   //"你說了:" + ReceivedMessage.events[0].message.text;                    
-                    //回覆用戶
-                    isRock.LineBot.Utility.ReplyMessage(ReceivedMessage.events[0].replyToken, Message, ChannelAccessToken);
-                }               
-                //回覆API OK
-                return Ok();
             }
             catch (Exception ex)
             {
-                return Ok();
+                LintBot.ReplyMessage(ReceivedMessage.events[0].replyToken, ex.Message);
             }
+            return;
         }
+        #endregion
     }
 }
---
+++ WebHook/Models/AirQulity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebHook.Models
+{
+    public class AirQulity
+    {
+        [Display(Name = "測站名稱")]
+        public string SiteName { get; set; }
+        [Display(Name = "縣市")]
+        public string County { get; set; }
+        [Display(Name = "空氣污染指標")]
+        public string PSI { get; set; }
+        [Display(Name = "指標污染物")]
+        public string MajorPollutant { get; set; }
+        [Display(Name = "狀態")]
+        public string Status { get; set; }
+        [Display(Name = "二氧化硫濃度")]
+        public string SO2 { get; set; }
+        [Display(Name = "一氧化碳濃度")]
+        public string CO { get; set; }
+        [Display(Name = "臭氧濃度")]
+        public string O3 { get; set; }
+        [Display(Name = "懸浮微粒濃度")]
+        public string PM10 { get; set; }
+        [Display(Name = "細懸浮微粒濃度")]
+        public string PM25 { get; set; }
+        [Display(Name = "二氧化氮濃度")]
+        public string NO2 { get; set; }
+        [Display(Name = "風速")]
+        public string WindSpeed { get; set; }
+        [Display(Name = "風向")]
+        public string WindDirec { get; set; }
+        [Display(Name = "FPMI")]
+        public string FPMI { get; set; }
+        [Display(Name = "NOX")]
+        public string NOx { get; set; }
+        [Display(Name = "NO")]
+        public string NO { get; set; }
+        [Display(Name = "發佈時間")]
+        public string PublishTime { get; set; }
+    }
+}
