5b6c32f0b591d56a932a478e4b1e5c84e1fca4c5
change.wu <change.wu@C4C015023>
2018-01-15 17:18:45 +0800

    O201801151718

---
+++ WebHook/Controllers/JobsController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebHook.Models;
+
+namespace WebHook.Controllers
+{
+    public class JobsController : ApiController
+    {
+        // GET: api/Jobs
+        public async void Get()
+        {
+            DateTime timeUtc = DateTime.UtcNow;
+            TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+            DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(timeUtc, cstZone);            
+            int hourly = cstTime.Hour;            
+
+            switch (hourly)
+            {
+                case 7:
+                    await Jobs.job_pm25_0700();
+                    break;
+                case 17:
+                    await Jobs.job_pm25_1700();
+                    break;
+                case 12:
+                    Jobs.job_exchange_rate_1200();
+                    break;
+                case 16:
+                    Jobs.job_exchange_rate_1630();
+                    break;
+                default:
+                    break;
+            }            
+        }
+    }
+}
